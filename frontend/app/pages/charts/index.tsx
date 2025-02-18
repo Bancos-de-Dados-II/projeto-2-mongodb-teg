9e393882e3c933db4d40e7112fd335ba4a13cd37
tasso.moreira <tassomoreira1@gmail.com>
2025-02-18 16:56:05 -0300

    feat(frontend): Create Chart component and integrate MongoDB Atlas iframe
    - Created `Chart` component to render MongoDB Atlas charts via iframe.
    - Styled `Chart` component to ensure it fits well within the page layout.
    - Integrated MongoDB Atlas chart iframe into the `Charts` page for data visualization.
    - Applied responsive design to ensure iframes are displayed correctly on various screen sizes.
    - Updated `Charts` page layout to accommodate the iframe and ensure proper display.

--- frontend/app/pages/charts/index.tsx
+++ frontend/app/pages/charts/index.tsx
@@ -1,7 +1,10 @@
 import type { LinksFunction } from "react-router";
 import type { Route } from "./+types/home";
 
+import "../../app.css";
 import styles from "./styles.css?url";
+
+import { Chart } from "~/components/chart";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -18,7 +21,24 @@ export function meta({}: Route.MetaArgs) {
 export default function Charts() {
   return (
     <main>
-      <h1>hello from the charts page</h1>
+      <h1>Charts</h1>
+      <div className="chartsList">
+        <Chart
+          src="https://charts.mongodb.com/charts-project-0-sqqjbel/embed/charts?id=f300c7df-2623-4723-b341-0e2659fd02c8&maxDataAge=3600&theme=light&autoRefresh=true"
+        />
+
+        <Chart
+          src="https://charts.mongodb.com/charts-project-0-sqqjbel/embed/charts?id=11bd5cfb-d65e-432e-8a92-8126b9f1578f&maxDataAge=3600&theme=light&autoRefresh=true"
+        />
+
+        <Chart
+          src="https://charts.mongodb.com/charts-project-0-sqqjbel/embed/charts?id=dca1897e-be0b-4447-9579-5c5ff40d35c6&maxDataAge=3600&theme=light&autoRefresh=true"
+        />
+
+        <Chart
+          src="https://charts.mongodb.com/charts-project-0-sqqjbel/embed/charts?id=21c4b358-89f9-40d9-91d8-5137d401cd6d&maxDataAge=3600&theme=light&autoRefresh=true"
+        />
+      </div>
     </main>
   );
 }
